3ce7a1a8d6fa1b25eab68fdcd7c77635dd514374
Eduardo Pires <eduardobrittopires@gmail.com>
2026-06-22 10:38:29 -0300

    Modular Front
    Criei uma seção apenas para a front da home, além disso organizei os arquivos de components em uma pasta "Home"

--- ide-site/src/app/page.tsx
+++ ide-site/src/app/page.tsx
@@ -1,40 +1,12 @@
-import Link from 'next/link';
 import styles from './page.module.css';
-import CTAHome from '../components/CTAHome/CTAHome';
+import CTAHome from '../components/Home/CTAHome/CTAHome';
+import FrontHome from '../components/Home/FrontHome/FrontHome';
 
 export default function Home() {
   return (
     <main className={styles.hero}>
-      <section className={styles.content}>
-        <div className={styles.textContent}>
-          <h1>
-            Transformamos Código em <span className={styles.highlight}>Impacto</span>.
-          </h1>
-
-          <p>
-            A IDE é a empresa júnio de informática da UFRGS. Desenvolvemos soluções digitais de alta
-            performance com a energia e inovação que só o ambiente acadêmico proporciona.
-          </p>
-
-          <div className={styles.actions}>
-            <Link href="/contato" className={`${styles.button} ${styles.buttonPrimary}`}>
-              Fale Conosco
-            </Link>
-            <Link href="/portfolio" className={`${styles.button} ${styles.buttonSecondary}`}>
-              Ver Portfólio
-            </Link>
-          </div>
-        </div>
-
-        <img
-          src="/imagens/detail-front.png"
-          alt="Detalhe frontal"
-          className={styles.heroImage}
-        />
-      </section>
-
+      <FrontHome />
       <CTAHome />
-
     </main>
   );
 }
---
+++ ide-site/src/components/Home/FrontHome/FrontHome.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from './FrontHome.module.css';
+
+export default function FrontHome() {
+  return (
+    <section className={styles.content}>
+      <div className={styles.textContent}>
+        <h1>
+          Transformamos Código em <span className={styles.highlight}>Impacto</span>.
+        </h1>
+
+        <p>
+          A IDE é a empresa júnior de informática da UFRGS. Desenvolvemos soluções digitais de alta
+          performance com a energia e inovação que só o ambiente acadêmico proporciona.
+        </p>
+
+        <div className={styles.actions}>
+          <Link href="/contato" className={`${styles.button} ${styles.buttonPrimary}`}>
+            Fale Conosco
+          </Link>
+          <Link href="/portfolio" className={`${styles.button} ${styles.buttonSecondary}`}>
+            Ver Portfólio
+          </Link>
+        </div>
+      </div>
+
+      <Image
+        src="/imagens/detail-front.png"
+        alt="Detalhe frontal"
+        className={styles.heroImage}
+        width={800}
+        height={700}
+      />
+    </section>
+  );
+}
