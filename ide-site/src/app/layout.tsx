ec1065d888ed7d99345bf72a41ab37a899bd14d1
marianairigaray <marianairigaray2004@gmail.com>
2026-07-02 14:58:12 -0300

    Adiciona fontes globais
    Adiciona as seguintes fontes de acordo com o Figma:
    - Fonte Título: Dela Gothic One
    - Fonte Principal: Bai Jamjuree
    - Fonte secundária: Open Sans

--- ide-site/src/app/layout.tsx
+++ ide-site/src/app/layout.tsx
@@ -1,15 +1,34 @@
 import './globals.css';
 import Navbar from '../components/Navbar/Navbar';
 import Footer from '../components/Footer/Footer';
+import { Dela_Gothic_One, Bai_Jamjuree, Open_Sans } from 'next/font/google';
+
+const delta_gothic_one = Dela_Gothic_One({
+  weight: ['400'],
+  subsets: ['latin'],
+  variable: '--font-titulo',
+})
+
+const bai_jamjuree = Bai_Jamjuree({
+  weight: ['400', '500', '700'],
+  subsets: ['latin'],
+  variable: '--font-principal',
+})
+
+const open_sans = Open_Sans({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--font-secundaria',
+})
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode;  
 }) {
   return (
     <html lang="pt-BR">
-      <body>
+      <body className={`${delta_gothic_one.variable} ${bai_jamjuree.variable} ${open_sans.variable}`}>
         <Navbar />
         {children}
         <Footer />
