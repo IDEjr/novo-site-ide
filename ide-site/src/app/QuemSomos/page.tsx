9d577d96290c98d613979812f094b6342fb02550
Eduardo Pires <eduardobrittopires@gmail.com>
2026-07-04 20:05:22 -0300

    CTA de Quem Somos
    Agora tem o showcase do processo seletivo e do manifesto na seção Quem Somos. É possível mexer nisso por meio de Parte nas pastas de componentes.

--- ide-site/src/app/QuemSomos/page.tsx
+++ ide-site/src/app/QuemSomos/page.tsx
@@ -1,6 +1,7 @@
 import styles from './page.module.css';
 import FrontQuemSomos from '@/components/QuemSomos/FrontQuemSomos';
 import NossaGente from '@/components/QuemSomos/NossaGente';
+import Parte from '@/components/QuemSomos/Parte';
 import Timeline from '@/components/QuemSomos/Timeline';
 
 export default function QuemSomos() {
@@ -9,6 +10,7 @@ export default function QuemSomos() {
       <FrontQuemSomos />
       <Timeline />
       <NossaGente />
+      <Parte />
     </main>
   );
 }
