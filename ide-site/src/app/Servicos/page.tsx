c2f5df5907bfe9f2f13ba59174ca8d71ae9507a6
marianairigaray <marianairigaray2004@gmail.com>
2026-07-10 19:30:35 -0300

    Implementacao do front da pagina de servicos

---
+++ ide-site/src/app/Servicos/page.tsx
@@ -0,0 +1,10 @@
+import FrontServicos from '@/src/components/Servicos/FrontServicos';
+import styles from './page.module.css';
+
+export default function Servicos() {
+  return (
+    <main className={styles.hero}>
+      <FrontServicos />
+    </main>
+  );
+}
