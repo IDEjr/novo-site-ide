d9a73d4edb6f7cdaf382028bd6b646c9eca957b8
marianairigaray <marianairigaray2004@gmail.com>
2026-07-13 15:33:39 -0300

    Implementacao do CTA da pagina de servicos

--- ide-site/src/app/Servicos/page.tsx
+++ ide-site/src/app/Servicos/page.tsx
@@ -1,10 +1,12 @@
 import FrontServicos from '@/src/components/Servicos/FrontServicos';
+import CTAServicos from '@/src/components/Servicos/CTAServicos';
 import styles from './page.module.css';
 
 export default function Servicos() {
   return (
     <main className={styles.hero}>
       <FrontServicos />
+      <CTAServicos />
     </main>
   );
 }
