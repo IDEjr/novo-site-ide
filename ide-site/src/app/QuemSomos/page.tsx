c0eb5b606a26db598863861ef9975e8338abdbdf
Eduardo Pires <eduardobrittopires@gmail.com>
2026-07-21 20:03:54 -0300

    QuemSomos animado
    Coloquei o FaultyTerminal com config personalizadas na seção QuemSomos

--- ide-site/src/app/QuemSomos/page.tsx
+++ ide-site/src/app/QuemSomos/page.tsx
@@ -1,16 +1,30 @@
-import styles from './page.module.css';
-import FrontQuemSomos from '@/components/QuemSomos/FrontQuemSomos/FrontQuemSomos';
-import NossaGente from '@/components/QuemSomos/NossaGente/NossaGente';
-import Parte from '@/components/QuemSomos/Parte/Parte';
-import Timeline from '@/components/QuemSomos/Timeline/Timeline';
+"use client";
+
+import styles from "./page.module.css";
+import FrontQuemSomos from "@/components/QuemSomos/FrontQuemSomos/FrontQuemSomos";
+import NossaGente from "@/components/QuemSomos/NossaGente/NossaGente";
+import Parte from "@/components/QuemSomos/Parte/Parte";
+import Timeline from "@/components/QuemSomos/Timeline/Timeline";
+import FaultyTerminal from "../../components/FaultyTerminal/FaultyTerminal";
 
 export default function QuemSomos() {
   return (
-    <main className={styles.hero}>
-      <FrontQuemSomos />
-      <Timeline />
-      <NossaGente />
-      <Parte />
-    </main>
+    <section className={styles.quemsomos}>
+      <div className={styles.background}>
+        <FaultyTerminal
+          scale={2.7}
+          tint="#143c69"
+          timeScale={0.3}
+          curvature={1.2}
+        />
+      </div>
+
+      <main className={styles.hero}>
+        <FrontQuemSomos />
+        <Timeline />
+        <NossaGente />
+        <Parte />
+      </main>
+    </section>
   );
 }
